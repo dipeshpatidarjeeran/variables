0ff7c0e378beabadcff61cf1df7d5b413c5ef0ab
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2021-12-05 17:02:41 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+# I am going to decleare a variables in python
+
+x = 6
+print(type(x))
+y = 5                 # x is a variable
+print(type(y))
+
+print( x + y )
+z = "ajay"
+print(type(z))
+fullname = "ritik patidar"   #fullname is str variable
+print(type(fullname))
+print(fullname)
+#  1x = 4 Invalid variables
+#  _x = 4 valid variables
+
+
+                         
+
